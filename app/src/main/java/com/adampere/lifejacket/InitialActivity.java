32cae7a449d99790b2be44c7f28206ff8e64a102
adam m pere <adampere001@gmail.com>
2016-05-23 15:52:21 -0400

    initial build

---
+++ app/src/main/java/com/adampere/lifejacket/InitialActivity.java
@@ -0,0 +1,53 @@
+package com.adampere.lifejacket;
+
+import android.content.Intent;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class InitialActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.adampere.lifejacket.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+        addRadioButtons(2);
+
+    }
+
+
+    /**
+     * Adds number radio buttons to the radio button group in this view.
+     * *** FUTURE: Update so that it adds a radio button for each camera
+     * @param number the number of radio buttons to add
+     */
+    public void addRadioButtons(int number) {
+            for (int i = 1; i <= number; i++) {
+                RadioButton rdbtn = new RadioButton(this);
+                rdbtn.setId((2) + i);
+                rdbtn.setText("Radio " + rdbtn.getId());
+                ((ViewGroup) findViewById(R.id.viewRadioGroup)).addView(rdbtn);
+            }
+    }
+
+    /**
+     * Called when the user presses the 'display view' button
+     * This function opens up the 'video stream' view with the stream from the
+     * appropriate camera/source
+     * @param view
+     */
+    public void openVideoStream(View view){
+        Intent intent =  new Intent(this, VideoStream.class);
+        RadioGroup rgp = (RadioGroup)findViewById(R.id.viewRadioGroup);
+        int radioID = rgp.getCheckedRadioButtonId();
+        intent.putExtra(EXTRA_MESSAGE, radioID);
+        startActivity(intent);
+    }
+
+}
